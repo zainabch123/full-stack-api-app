88d9bb8f33e1c1673caee4030c11739819836bac
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-03-18 12:28:23 +0000

    added basic structure and packages

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+//Load environment variable
+import { config } from "dotenv";
+config();
+
+// Import express and cors
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+// Set up express
+const app = express();
+app.disable("x-powered-by");
+app.use(cors());
+// Tell express to use a JSON parser middleware
+app.use(express.json());
+// Tell express to use a URL Encoding middleware
+app.use(express.urlencoded({ extended: true }));
+
+
+//Add routers below:
+
+
+// Set up a default "catch all" route to use when someone visits a route
+// that we haven't built
+app.get("*", (req, res) => {
+  res.json({ ok: true });
+});
+
+// Start API server
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`\n Server is running on http://localhost:${port}\n`);
+});
